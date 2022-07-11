c3bc480cc48c084e704770750fc13cdb19721e1f
Sergio H <sergiohernandezroman@gmail.com>
2022-07-11 13:45:48 +0200

    add testing utils

---
+++ src/utils/test.utils.tsx
@@ -0,0 +1,25 @@
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+function render(
+  ui: JSX.Element,
+  { preloadedState, reducer }: { preloadedState: {}; reducer: {} }
+) {
+  function Wrapper({ children }: { children: JSX.Element }) {
+    return (
+      <Provider
+        store={configureStore({
+          reducer,
+          preloadedState,
+        })}
+      >
+        {children}
+      </Provider>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper });
+}
+
+export * from '@testing-library/react';
+export { render };
